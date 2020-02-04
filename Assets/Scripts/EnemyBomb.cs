5fa4308c0e7b65fdcdb73a2b361fa137e61d53a6
1dmaol <marc97oller@gmail.com>
2020-02-04 16:29:19 +0100

    first commit marc drop bomb and behaviour
    a

--- Assets/Scripts/EnemyBomb.cs
+++ Assets/Scripts/EnemyBomb.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EnemyBomb : MonoBehaviour
 {
+    public float Timer = 1;
+    protected float temp = 1;
+
+    public GameObject bomb = null;
+
+    public GameObject follow = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,21 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    temp -= Time.deltaTime;
+     if (temp <= 0f)
+     {
+         
+        dropBomb();
+         temp = Timer;
+     }
+    }
+
+
+
+    void dropBomb(){
+        Vector3 position = follow.transform.position;
+        position.y = 1;
+        position.x = position.x + 0.5f;
+        Instantiate(bomb, position, Quaternion.identity);
     }
 }
